f78aa561f7f5b7bb8b6fb750f3365d3f7deb18a7
yangqian <linsenwang104@gmail.com>
2025-09-09 20:59:46 +0800

    Deployed  with MkDocs version: 1.6.1

--- javascripts/katex.js
+++ javascripts/katex.js
@@ -3,8 +3,8 @@ document$.subscribe(({ body }) => {
     delimiters: [
       { left: "$$",  right: "$$",  display: true },
       { left: "$",   right: "$",   display: false },
-      { left: "\\(", right: "\\)", display: false },
-      { left: "\\[", right: "\\]", display: true }
+      { left: "\$", right: "\$", display: false },
+      { left: "\$$", right: "\$$", display: true }
     ],
   })
 })
